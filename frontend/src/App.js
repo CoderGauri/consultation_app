b1159f046d2c1d4ecfef06e8a161e5d55a938da4
Gauri <gauriM304@gmail.com>
2024-04-10 17:15:00 +0530

    hook added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(name , skill);
-    addConsultant({ name, skill });
+    //addConsultant({ name, skill });
     setName('');
     setSkill('');
   };
