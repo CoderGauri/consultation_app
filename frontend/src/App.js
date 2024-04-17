7e4749e6c88683b84f09571fd631a38edacbd53b
Gauri <gauriM304@gmail.com>
2024-04-17 11:02:21 +0530

    Get consultants

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [name, setName] = useState('');
-  const [skills, setSkill] = useState('');
-  const[cost , setCost] = useState('');
+  const [name, setName] = useState("");
+  const [skills, setSkill] = useState("");
+  const [cost, setCost] = useState("");
   const [consultants, setConsultants] = useState([]);
   let id = 1;
 
@@ -15,68 +15,79 @@ function App() {
 
   const fetchPosts = async () => {
     try {
-      const response = await axios.get('https://consultation-app-be.onrender.com/api/consultants');
-      console.log(response.data)
+      const response = await axios.get(
+        "https://consultation-app-be.onrender.com/api/consultants"
+      );
+      console.log(response.data);
       setConsultants(response.data);
     } catch (error) {
-      console.error('Error fetching posts:', error);
+      console.error("Error fetching posts:", error);
     }
   };
+
+ 
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name , skills , cost); 
-    id++
-    
-    
-    setConsultants([...consultants , { name, skills ,cost , id}]);
-    setName('');
-    setSkill('');
-    setCost('');
+    console.log(name, skills, cost);
+    id++;
+
+    setConsultants([...consultants, { name, skills, cost, id }]);
+    setName("");
+    setSkill("");
+    setCost("");
   };
 
-  const handleDelete = (index) => {
-    const updatedPosts = [...consultants];
-    updatedPosts.splice(index, 1);
-    setConsultants(updatedPosts);
-  };
-  return (
-    <>
+  
+
+
+return (
+  <>
     <div>
-    <form onSubmit={handleSubmit}>
-    <div className="form-group">
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)}required />
-      </label>
-      <label>
-        Skill:
-        <input type="text" value={skills} onChange={(e) => setSkill(e.target.value)}required />
-      </label>
-      <label>
-        Cost per hour:
-        <input type="text" value={cost} onChange={(e) => setCost(e.target.value)} required/>
-      </label>
-      <button type="submit">Add Consultant</button>
-      </div>
-    </form>
-
-
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Skill:
+            <input
+              type="text"
+              value={skills}
+              onChange={(e) => setSkill(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Cost per hour:
+            <input
+              type="text"
+              value={cost}
+              onChange={(e) => setCost(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Add Consultant</button>
+        </div>
+      </form>
     </div>
     <div>
-  
-    {consultants.map((consultants, index) => (
-            <div key={index}>
-              <h3>{consultants.name}</h3>
-              <p>{consultants.skills}</p>
-              <p>{consultants.cost}</p>
-              <button onClick={() => handleDelete(index)}>Delete</button>
-             
-            </div>
-          ))}
-  
-</div>
-    </>
-  );
+      {consultants.map((consultants, index) => (
+        <div key={index}>
+          <h3>{consultants.name}</h3>
+          <p>{consultants.skills}</p>
+          <p>{consultants.cost}</p>
+          {/* <button onClick={() => handleDelete(index)}>Delete</button> */}
+        </div>
+      ))}
+    </div>
+  </>
+);
 }
-
 export default App;
